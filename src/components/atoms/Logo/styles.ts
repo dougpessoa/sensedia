2bfc750cb21e1d7be3c4de612af88ae56b9a7cff
Doug Pessoa <douglaspereira1@outlook.com>
2022-03-10 13:33:42 -0300

    feat: chromatic deploy and change url svg images

--- src/components/atoms/Logo/styles.ts
+++ src/components/atoms/Logo/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { LogoTypes } from ".";
 
 export const Img = styled.img.attrs({
-  src: "/icons/logo.svg",
+  src: "https://sensedia.vercel.app/icons/logo.svg",
 })<LogoTypes>`
   max-width: ${({ size }) => `${size}px`};
 `;
--- src/styles/icons.tsx
+++ src/styles/icons.tsx
@@ -6,4 +6,6 @@ export {
 export { Search, LoaderAlt } from "@styled-icons/boxicons-regular/";
 export { ListUl } from "@styled-icons/bootstrap/ListUl";
 
-export const GridIcon = () => <img src="/icons/grid.svg" />;
+export const GridIcon = () => (
+  <img src="https://sensedia.vercel.app/icons/grid.svg" />
+);
